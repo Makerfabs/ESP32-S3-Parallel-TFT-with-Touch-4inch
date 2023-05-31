620a1cba444dda554a0e1f0bed863cb58832f391
一九五 <2259761058@qq.com>
2023-05-31 18:19:47 +0800

    Add mpu6050 demo

---
+++ example/mpu6050_demo/mpu6050_demo.ino
@@ -0,0 +1,164 @@
+// Basic demo for accelerometer readings from Adafruit MPU6050
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include "Arduino_GFX_Library.h"
+
+#define GFX_BL -1
+
+#define I2C_SDA_PIN 17
+#define I2C_SCL_PIN 18
+
+Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
+    1 /* CS */, 12 /* SCK */, 11 /* SDA */,
+    45 /* DE */, 4 /* VSYNC */, 5 /* HSYNC */, 21 /* PCLK */,
+    39 /* R0 */, 40 /* R1 */, 41 /* R2 */, 42 /* R3 */, 2 /* R4 */,
+    0 /* G0/P22 */, 9 /* G1/P23 */, 14 /* G2/P24 */, 47 /* G3/P25 */, 48 /* G4/P26 */, 3 /* G5 */,
+    6 /* B0 */, 7 /* B1 */, 15 /* B2 */, 16 /* B3 */, 8 /* B4 */
+);
+
+Arduino_ST7701_RGBPanel *gfx = new Arduino_ST7701_RGBPanel(
+    bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */,
+    true /* IPS */, 480 /* width */, 480 /* height */,
+    st7701_type1_init_operations, sizeof(st7701_type1_init_operations),
+    true /* BGR */);
+
+Adafruit_MPU6050 mpu;
+
+int x = 0;
+int y = 0;
+
+void setup(void)
+{
+    Serial.begin(115200);
+    Serial.println("Adafruit MPU6050 test!");
+
+    pinMode(GFX_BL, OUTPUT);
+    digitalWrite(GFX_BL, HIGH);
+
+    gfx->begin();
+    gfx->fillScreen(BLACK);
+
+    Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+
+    // Try to initialize!
+    if (!mpu.begin())
+    {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1)
+        {
+            delay(10);
+        }
+    }
+    Serial.println("MPU6050 Found!");
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange())
+    {
+    case MPU6050_RANGE_2_G:
+        Serial.println("+-2G");
+        break;
+    case MPU6050_RANGE_4_G:
+        Serial.println("+-4G");
+        break;
+    case MPU6050_RANGE_8_G:
+        Serial.println("+-8G");
+        break;
+    case MPU6050_RANGE_16_G:
+        Serial.println("+-16G");
+        break;
+    }
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange())
+    {
+    case MPU6050_RANGE_250_DEG:
+        Serial.println("+- 250 deg/s");
+        break;
+    case MPU6050_RANGE_500_DEG:
+        Serial.println("+- 500 deg/s");
+        break;
+    case MPU6050_RANGE_1000_DEG:
+        Serial.println("+- 1000 deg/s");
+        break;
+    case MPU6050_RANGE_2000_DEG:
+        Serial.println("+- 2000 deg/s");
+        break;
+    }
+
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth())
+    {
+    case MPU6050_BAND_260_HZ:
+        Serial.println("260 Hz");
+        break;
+    case MPU6050_BAND_184_HZ:
+        Serial.println("184 Hz");
+        break;
+    case MPU6050_BAND_94_HZ:
+        Serial.println("94 Hz");
+        break;
+    case MPU6050_BAND_44_HZ:
+        Serial.println("44 Hz");
+        break;
+    case MPU6050_BAND_21_HZ:
+        Serial.println("21 Hz");
+        break;
+    case MPU6050_BAND_10_HZ:
+        Serial.println("10 Hz");
+        break;
+    case MPU6050_BAND_5_HZ:
+        Serial.println("5 Hz");
+        break;
+    }
+
+    Serial.println("");
+    delay(100);
+}
+
+void loop()
+{
+
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    /* Print out the values */
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+
+    Serial.println("");
+
+    draw_ball(a.acceleration.x, a.acceleration.y);
+    // delay(500);
+}
+
+void draw_ball(float r_x, float r_y)
+{
+    gfx->fillCircle(x, y, 20, BLACK);
+
+    x = (x + (int)(240 + r_x * 24)) / 2;
+    y = (y + (int)(240 + r_y * 24)) / 2;
+
+    gfx->fillCircle(x, y, 20, RED);
+}
